8aa198e0000f80488df1eaf2d767c918c664d106
Maksym <46656676+Zu9zwan9@users.noreply.github.com>
2021-05-14 02:17:48 +0300

    Delete currency-project directory

--- currency-project/src/main/java/currency/project/dao/AdminDao.java
+++
@@ -1,9 +0,0 @@
-package currency.project.dao;
-
-import currency.project.model.Admin;
-
-import java.util.Optional;
-
-public interface AdminDao {
-    Optional<Admin> getByLogin(String login);
-}
--- currency-project/src/main/java/currency/project/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package currency.project.service;
-
-import currency.project.model.Admin;
-
-import java.util.Optional;
-
-public interface AdminService {
-    Optional<Admin> getByLogin(String login);
-}
